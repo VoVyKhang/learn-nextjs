c7778e1d9bc70e6db7b4b9cacc2ab32b105d2e49
VoVyKhang <vo.vykhang24@gmail.com>
2023-06-14 23:29:04 +0700

    [ADD]: SSG getStaticProps with Typescript

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,17 @@
+import { useRouter } from 'next/router'
 import * as React from 'react'
 
 export interface IAppProps {}
 
 export default function App(props: IAppProps) {
+  const router = useRouter()
+
+  console.log('Query: ', router.query)
+
+  // React.useEffect(() => {
+  //   // Check query available
+  // })
+
   return (
     <div>
       <h2>About Page</h2>
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,11 +1,37 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next'
 import * as React from 'react'
 
-export interface IAppProps {}
+export interface IGetListPostProps {
+  posts: any[]
+}
 
-export default function App(props: IAppProps) {
+export default function GetListPost({ posts }: IGetListPostProps) {
   return (
     <div>
-      <h2>List Post Page</h2>
+      <h1>Post List</h1>
+      <ul>
+        {posts.map((post) => {
+          return <li key={post.id}>{post.title}</li>
+        })}
+      </ul>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps<IGetListPostProps> = async (
+  context: GetStaticPropsContext
+) => {
+  const postsList = await fetch(
+    'https://js-post-api.herokuapp.com/api/posts?_page=1'
+  )
+  const data = await postsList.json()
+
+  return {
+    props: {
+      posts: data.data.map((rs: any) => ({
+        id: rs.id,
+        title: rs.title,
+      })),
+    },
+  }
 }
